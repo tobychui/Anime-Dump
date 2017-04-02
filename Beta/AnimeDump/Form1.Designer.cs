4eb16512e318437d432014d1609bb79d7e724e0d
Toby Chui <tobychui@users.noreply.github.com>
2017-04-02 15:46:06 +0800

    Beta version (stable build)
    Build with only the simplest form of exception handling.

---
+++ Beta/AnimeDump/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace AnimeDump
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1065, 20);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 20);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1032, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "M:\\download\\";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(20, 67);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(479, 604);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(512, 222);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 268);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Packup\r\n>>>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(677, 68);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox2.Size = new System.Drawing.Size(511, 604);
+            this.listBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(512, 582);
+            this.button3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 92);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Delete Pending";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(1200, 68);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox3.Size = new System.Drawing.Size(297, 604);
+            this.listBox3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1263, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Delete Pending Animes";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(512, 491);
+            this.button4.Margin = new System.Windows.Forms.Padding(5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(155, 38);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1517, 703);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Anime Dump";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Beta/AnimeDump/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimeDump
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(textBox1.Text + @"DeleteThis\") == false)
+            {
+                Directory.CreateDirectory(textBox1.Text + @"DeleteThis\");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateListbox();
+        }
+
+        private void UpdateListbox()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+
+            DirectoryInfo d = new DirectoryInfo(textBox1.Text);//Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.mp4"); //Getting Text files
+            foreach (FileInfo file in Files)
+            {
+                listBox1.Items.Add(file.Name);
+            }
+
+            string[] folders = System.IO.Directory.GetDirectories(textBox1.Text, "*", System.IO.SearchOption.AllDirectories);
+            foreach (string str in folders)
+            {
+                listBox2.Items.Add(str.Replace(textBox1.Text, ""));
+            }
+
+            DirectoryInfo d2 = new DirectoryInfo(textBox1.Text + @"DeleteThis\");//Assuming Test is your Folder
+            FileInfo[] Files2 = d2.GetFiles("*.mp4"); //Getting Text files
+            foreach (FileInfo file1 in Files2)
+            {
+                listBox3.Items.Add(file1.Name);
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (var item in listBox1.SelectedItems)
+            {
+                File.Move(textBox1.Text + item.ToString(), textBox1.Text + @"DeleteThis\" + item.ToString());
+            }
+            UpdateListbox();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            string FolderName = "";
+            foreach (var item in listBox1.SelectedItems)
+            {
+                if (num == 0)
+                {
+                    string input = Microsoft.VisualBasic.Interaction.InputBox("Please name your folder.", "Packup Tools", item.ToString(), -1, -1);
+                    FolderName = input;
+                    if (FolderName == "")
+                    {
+                        MessageBox.Show("Operation Cancelled");
+                        return;
+                    }else
+                    {
+                        if (Directory.Exists(textBox1.Text + FolderName + @"\") == false)
+                        {
+                            Directory.CreateDirectory(textBox1.Text + FolderName + @"\");
+                        }
+
+                    }
+                }
+                try
+                {
+                    File.Move(textBox1.Text + item.ToString(), textBox1.Text + FolderName + @"\" + item.ToString());
+                }catch
+                {
+                    MessageBox.Show("Some Error has occured while moving the file " + item.ToString());
+                }
+               
+                num++;
+            }
+            UpdateListbox();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            UpdateListbox();
+        }
+    }
+}
